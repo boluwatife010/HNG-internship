a51161324f1f5737260c810cfc37e4a8b18cc7a1
boluwatife010 <temitopeboluojo@gmail.com>
2023-09-07 18:04:06 +0100

    stage one upgrade1

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 const app = express();
 app.use(bodyParser.json());
 app.get('/api', (req:express.Request, res:express.Response) => {
-    const {slackName, slackTrack} = req.params;
+    const {slackName, slackTrack} = req.query;
     const currentDay = new Date().toLocaleString('en-US', {weekday: 'long'});
     const currentTime = new Date().toISOString();
     const gitHubFileUrl = 'https://github.com/boluwatife010/Hackerank-solutions/blob/main/dayOfProgrammer.js';
